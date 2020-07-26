391f7d2c535b9afff0b12bd1c07503db350d87bf
Shinichi Kawamura <sttaf34@gmail.com>
2020-07-26 18:53:10 +0900

    モーダルで入力された値をモーダル元で使うサンプルの追加

--- src/apps/ModalApp.tsx
+++ src/apps/ModalApp.tsx
@@ -3,6 +3,7 @@ import {
   Text,
   Modal,
   Button,
+  TextInput,
   StatusBar,
   StyleSheet,
   SafeAreaView,
@@ -12,33 +13,55 @@ const styles = StyleSheet.create({
   safeAreaView: {
     flex: 1,
   },
+  input: {
+    margin: 12,
+    height: 40,
+    borderColor: "pink",
+    borderWidth: 1,
+  },
 })
 
 type ModalViewProps = {
   isVisible: boolean
   setVisible: React.Dispatch<React.SetStateAction<boolean>>
+  value: string
+  setValue: React.Dispatch<React.SetStateAction<string>>
 }
 
 const ModalView: React.FC<ModalViewProps> = (props: ModalViewProps) => {
-  const { isVisible, setVisible } = props
+  const { isVisible, setVisible, value, setValue } = props
 
   return (
     <Modal animationType="slide" visible={isVisible}>
-      <Text>Hello Modal!</Text>
-      <Button title="Close!" onPress={() => setVisible(false)} />
+      <SafeAreaView style={styles.safeAreaView}>
+        <Text>Hello Modal!</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={(text) => setValue(text)}
+          value={value}
+        />
+        <Button title="Close!" onPress={() => setVisible(false)} />
+      </SafeAreaView>
     </Modal>
   )
 }
 
 export const ModalApp: React.FC = () => {
   const [modalVisible, setModalVisible] = useState(false)
+  const [value, setValue] = React.useState("") // モーダルの中で入力される用
 
   return (
     <>
-      <ModalView isVisible={modalVisible} setVisible={setModalVisible} />
+      <ModalView
+        isVisible={modalVisible}
+        setVisible={setModalVisible}
+        value={value}
+        setValue={setValue}
+      />
 
       <SafeAreaView style={styles.safeAreaView}>
         <StatusBar hidden={false} />
+        <Text>{value}</Text>
         <Button title="Open!" onPress={() => setModalVisible(true)} />
       </SafeAreaView>
     </>
