2e5b53280e8cb52c713a75bed6a3ac75c9ba51b3
Shinichi Kawamura <sttaf34@gmail.com>
2020-08-26 01:51:43 +0900

    TouchableHighlight の中が複雑な場合のサンプル追加

--- src/screens/PressableScreen.tsx
+++ src/screens/PressableScreen.tsx
@@ -2,6 +2,7 @@
 
 import React from "react"
 import { View, Text, StyleSheet, TouchableHighlight } from "react-native"
+import { Divider } from "react-native-elements"
 
 // Pressable は Expo SDK 39 で多分使えるようになる
 
@@ -14,19 +15,38 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 4,
   },
+  divider: {
+    margin: 5,
+  },
 })
 
 export const PressableScreen: React.FC = () => {
   return (
-    <View>
-      <TouchableHighlight
-        style={styles.button}
-        activeOpacity={0.6}
-        underlayColor="#DDDDDD"
-        onPress={() => alert("Pressed!")}
-      >
-        <Text style={styles.text}>ボタン</Text>
-      </TouchableHighlight>
-    </View>
+    <>
+      <View>
+        <TouchableHighlight
+          style={styles.button}
+          activeOpacity={0.6}
+          underlayColor="#DDDDDD"
+          onPress={() => alert("Pressed!")}
+        >
+          <Text style={styles.text}>ボタン</Text>
+        </TouchableHighlight>
+      </View>
+      <View>
+        <TouchableHighlight
+          style={styles.button}
+          activeOpacity={0.6}
+          underlayColor="#DDDDDD"
+          onPress={() => alert("Pressed!")}
+        >
+          <View>
+            <Text style={styles.text}>ボタン</Text>
+            <Divider style={styles.divider} />
+            <Text style={styles.text}>ボタン</Text>
+          </View>
+        </TouchableHighlight>
+      </View>
+    </>
   )
 }
