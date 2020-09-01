ba1b0e3c9d579815e233d8d98be2617af0cd718c
Shinichi Kawamura <sttaf34@gmail.com>
2020-09-02 06:32:30 +0900

    セルを削除するアニメーションのサンプルを追加

--- src/screens/list/SwipeAnimatedScreen.tsx
+++ src/screens/list/SwipeAnimatedScreen.tsx
@@ -1,88 +1,220 @@
 import React from "react"
 import {
+  View,
   Text,
+  Easing,
   Button,
   Animated,
   StyleSheet,
   ListRenderItemInfo,
 } from "react-native"
 import { SwipeListView } from "react-native-swipe-list-view"
+import { MarginText } from "src/components/MarginText"
 
-type Fruit = {
-  id: string
-  name: string
-}
-
-const initialFruits: Fruit[] = [
-  { id: "1", name: "apple" },
-  { id: "2", name: "banana" },
-  { id: "3", name: "coconut" },
-]
+const cellHeight = 50
 
 const styles = StyleSheet.create({
   renderItem: {
-    alignItems: "center",
-    backgroundColor: "#CCC",
+    backgroundColor: "#ffffff",
     justifyContent: "center",
-    height: 50,
+    height: cellHeight,
   },
   renderHiddenItem: {
     alignItems: "center",
     flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
-    paddingLeft: 15,
-    paddingRight: 15,
-    height: 50,
+    backgroundColor: "#ff0000",
+    paddingRight: 5,
+    height: cellHeight,
+  },
+  buttonContainer: {
+    borderWidth: 0.5,
   },
 })
 
-const opacityValues = Array.from({ length: 3 }).map(() => new Animated.Value(1))
+type Fruit = {
+  key: string
+  name: string
+}
+
+const initialFruits: Fruit[] = [
+  { key: "1", name: "apple" },
+  { key: "2", name: "banana" },
+  { key: "3", name: "coconut" },
+  { key: "4", name: "durian" },
+  { key: "5", name: "fig" },
+  { key: "6", name: "grape" },
+  { key: "7", name: "kiwi" },
+  { key: "8", name: "lemon" },
+  { key: "9", name: "lime" },
+  { key: "10", name: "mango" },
+  { key: "11", name: "melon" },
+  { key: "12", name: "orange" },
+  { key: "13", name: "papaya" },
+  { key: "14", name: "peach" },
+  { key: "15", name: "pear" },
+  { key: "16", name: "plum" },
+  { key: "17", name: "pineapple" },
+  { key: "18", name: "strawberry" },
+]
+
+const useAnimationsHeightDown = (keys: string[], height: number) => {
+  const values: Record<string, Animated.Value | undefined> = {}
+  keys.forEach((key) => {
+    values[key] = new Animated.Value(0)
+  })
+
+  const config: Animated.TimingAnimationConfig = {
+    toValue: 1,
+    duration: 1000,
+    useNativeDriver: true,
+    easing: Easing.out(Easing.exp),
+  }
+
+  const start = (key: string, endCallback: Animated.EndCallback) => {
+    const value = values[key]
+    if (value) {
+      Animated.timing(value, config).start(endCallback)
+    }
+  }
+
+  const styleAnimations = (key: string) => {
+    const value = values[key]
+    if (value === undefined) {
+      return {}
+    }
+
+    const scaleY = 0
+    const translateY = (height * scaleY - height) / 2
+
+    const interPolateTranslateY = value.interpolate({
+      inputRange: [0, 1],
+      outputRange: [0, translateY],
+    })
+    const interPolateScaleY = value.interpolate({
+      inputRange: [0, 1],
+      outputRange: [1, scaleY],
+    })
+
+    return {
+      transform: [
+        { translateY: interPolateTranslateY },
+        { scaleY: interPolateScaleY },
+      ],
+    }
+  }
+
+  return { styleAnimations, start }
+}
+
+const useAnimationsMoveUp = (keys: string[], height: number) => {
+  const values: Record<string, Animated.Value | undefined> = {}
+  keys.forEach((key) => {
+    values[key] = new Animated.Value(0)
+  })
+
+  const config: Animated.TimingAnimationConfig = {
+    toValue: 1,
+    duration: 1000,
+    useNativeDriver: true,
+    easing: Easing.out(Easing.exp),
+  }
+
+  const start = (key: string) => {
+    const value = values[key]
+    if (value) {
+      Animated.timing(value, config).start()
+    }
+  }
+
+  const styleAnimations = (key: string) => {
+    const value = values[key]
+    if (value === undefined) {
+      return {}
+    }
+
+    const interPolateTranslateY = value.interpolate({
+      inputRange: [0, 1],
+      outputRange: [0, -height],
+    })
+
+    return {
+      transform: [{ translateY: interPolateTranslateY }],
+    }
+  }
+
+  return { styleAnimations, start }
+}
 
 export const SwipeAnimatedScreen: React.FC = () => {
   const [fruits, setFruits] = React.useState(initialFruits)
+  const keys = fruits.map((fruit) => fruit.key)
+  const useHeightDown = useAnimationsHeightDown(keys, cellHeight)
+  const useMoveUp = useAnimationsMoveUp(keys, cellHeight)
 
-  const renderItem = (info: ListRenderItemInfo<Fruit>): JSX.Element => {
-    const { item, index } = info
-    const opacityValue = opacityValues[index]
-    return (
-      <Animated.View style={[styles.renderItem, { opacity: opacityValue }]}>
-        <Text>
-          {index}. {item.name}
-        </Text>
-      </Animated.View>
-    )
-  }
-
-  const onPress = () => {
-    const deleteIndex = fruits.findIndex((fruit) => fruit.id === "3")
-    const opacityValue = opacityValues[deleteIndex]
+  const deleteFruit = (key: string) => {
+    const deleteIndex = fruits.findIndex((fruit) => fruit.key === key)
     if (deleteIndex < 0) {
       return
     }
 
-    const config: Animated.TimingAnimationConfig = {
-      toValue: 0,
-      duration: 1500,
-      useNativeDriver: false,
-    }
-
-    Animated.timing(opacityValue, config).start(() => {
+    // 削除する果物は高さをゼロにするアニメーション
+    useHeightDown.start(key, () => {
       const slicedFruits = fruits.slice()
       slicedFruits.splice(deleteIndex, 1)
       setFruits(slicedFruits)
     })
+
+    // 削除する果物より下に位置するものは移動のアニメーション
+    fruits.forEach((fruit, index) => {
+      if (deleteIndex < index) {
+        useMoveUp.start(fruit.key)
+      }
+    })
+  }
+
+  const renderItem = (info: ListRenderItemInfo<Fruit>): JSX.Element => {
+    const { item: fruit } = info
+    return (
+      <Animated.View
+        style={[
+          styles.renderItem,
+          useHeightDown.styleAnimations(fruit.key),
+          useMoveUp.styleAnimations(fruit.key),
+        ]}
+      >
+        <MarginText>{fruit.name}</MarginText>
+      </Animated.View>
+    )
+  }
+
+  const renderHiddenItem = (info: ListRenderItemInfo<Fruit>): JSX.Element => {
+    const { item: fruit } = info
+    return (
+      <Animated.View
+        style={[
+          styles.renderHiddenItem,
+          useHeightDown.styleAnimations(fruit.key),
+          useMoveUp.styleAnimations(fruit.key),
+        ]}
+      >
+        <Text> </Text>
+        <View style={styles.buttonContainer}>
+          <Button title="Hey!" onPress={() => deleteFruit(fruit.key)} />
+        </View>
+      </Animated.View>
+    )
   }
 
   return (
     <>
-      <Button title="削除" onPress={onPress} />
       <SwipeListView
-        disableLeftSwipe
-        disableRightSwipe
         data={fruits}
-        keyExtractor={(fruit) => fruit.id}
+        keyExtractor={(fruit) => fruit.key}
         renderItem={renderItem}
+        renderHiddenItem={renderHiddenItem}
+        rightOpenValue={-66}
       />
     </>
   )
