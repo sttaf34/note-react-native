d78d255fe3af27ae52f22f91430cde6b1f654360
Shinichi Kawamura <sttaf34@gmail.com>
2020-09-04 09:11:52 +0900

    AsyncStorage のサンプルを追加

--- src/screens/other/AsyncStorageScreen.tsx
+++ src/screens/other/AsyncStorageScreen.tsx
@@ -1,13 +1,23 @@
 import React from "react"
+import firebase from "firebase"
 import { Button, ScrollView } from "react-native"
 import AsyncStorage from "@react-native-community/async-storage"
 
 const KEY_FOR_STRING = "KEY_FOR_STRING"
 const KEY_FOR_OBJECTS = "KEY_FOR_OBJECTS"
+const KEY_FOR_FIREBASE = "KEY_FOR_FIREBASE"
 
 type Score = {
   id: string
   value: number
+}
+
+type Vegetable = {
+  id: string
+  name: string
+  price: number
+  createdAt: firebase.firestore.Timestamp
+  updatedAt: firebase.firestore.Timestamp
 }
 
 export const AsyncStorageScreen: React.FC = () => {
@@ -58,12 +68,69 @@ export const AsyncStorageScreen: React.FC = () => {
     }
   }
 
+  const setFirebaseObjectToStorage = async () => {
+    const vegetable: Vegetable = {
+      id: String(Math.random()),
+      name: "大根",
+      price: 198,
+      createdAt: firebase.firestore.Timestamp.now(),
+      updatedAt: firebase.firestore.Timestamp.now(),
+    }
+    console.log(vegetable)
+
+    try {
+      const jsonString = JSON.stringify(vegetable)
+      await AsyncStorage.setItem(KEY_FOR_FIREBASE, jsonString)
+      console.log("SAVE")
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const getFirebaseObjectFromStorage = async () => {
+    try {
+      const jsonString = await AsyncStorage.getItem(KEY_FOR_FIREBASE)
+      if (jsonString) {
+        const object = JSON.parse(jsonString)
+        // new しないとインスタンスメソッドが使えない
+        const createdAt = new firebase.firestore.Timestamp(
+          object.createdAt.seconds,
+          object.createdAt.nanoseconds
+        )
+        const updatedAt = new firebase.firestore.Timestamp(
+          object.updatedAt.seconds,
+          object.updatedAt.nanoseconds
+        )
+        const vegetable: Vegetable = {
+          id: object.id,
+          name: object.name,
+          price: object.price,
+          createdAt,
+          updatedAt,
+        }
+        console.log(vegetable.createdAt.toDate())
+      } else {
+        console.log(jsonString || "NULL!")
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <ScrollView>
       <Button title="setStringToStorage" onPress={setStringToStorage} />
       <Button title="getStringFromStorage" onPress={getStringFromStorage} />
       <Button title="setObjectsToStorage" onPress={setObjectsToStorage} />
       <Button title="getObjectsFromStorage" onPress={getObjectsFromStorage} />
+      <Button
+        title="setFirebaseObjectToStorage"
+        onPress={setFirebaseObjectToStorage}
+      />
+      <Button
+        title="getFirebaseObjectFromStorage"
+        onPress={getFirebaseObjectFromStorage}
+      />
     </ScrollView>
   )
 }
