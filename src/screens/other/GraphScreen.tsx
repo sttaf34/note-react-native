14c4cfb2f9b0d9bd1556e09bd0b10d2722ab16a2
Shinichi Kawamura <sttaf34@gmail.com>
2020-12-08 13:57:58 +0900

    グラフのサンプルを追加

--- src/screens/other/GraphScreen.tsx
+++ src/screens/other/GraphScreen.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import dayjs from "dayjs"
-import { View, ScrollView } from "react-native"
+import { ScrollView } from "react-native"
 import { VictoryLine, VictoryChart, VictoryAxis } from "victory-native"
 
 import { MarginDiveder } from "src/components/MarginDiveder"
@@ -40,22 +40,61 @@ const VictoryHelloWithAxis: React.FC = () => {
 }
 
 const VictoryDateAxis: React.FC = () => {
-  const startDate = dayjs("2020-06-25 04:00:58")
-  const endDate = dayjs("2020-08-04 04:00:58")
-
-  const data = [
-    { x: startDate, y: Math.random() },
-    { x: dayjs("2020-07-25 04:00:58"), y: Math.random() },
-    { x: endDate, y: Math.random() },
+  // 横軸を時刻にした場合、厳密に反映される
+  const dates = [
+    dayjs("2020-06-25"),
+    dayjs("2020-06-26"),
+    dayjs("2020-06-27"),
+    dayjs("2020-06-28"),
+    dayjs("2020-06-29"),
+    dayjs("2020-08-04"),
+    dayjs("2020-08-05"),
+    dayjs("2020-08-25"),
+    dayjs("2020-08-24"),
+    dayjs("2020-08-26"),
+    dayjs("2020-08-28"),
   ]
+
+  const data = dates.map((aDate) => {
+    return { x: aDate, y: Math.random() }
+  })
 
   return (
     <VictoryChart>
       <VictoryAxis
-        tickValues={[startDate, endDate]}
+        tickValues={[dates[0], dates[dates.length - 1]]}
         tickFormat={(aDate: dayjs.Dayjs) => aDate.format("YYYY/MM/DD")}
       />
       <VictoryAxis dependentAxis domain={[0, 1]} />
+      <VictoryLine data={data} x="x" y="y" />
+    </VictoryChart>
+  )
+}
+
+const VictorySampleData: React.FC = () => {
+  // 筋トレの何かの種目が順調に伸びていくようなサンプルデータ
+  const data = [
+    { x: dayjs("2020-01-01"), y: 45 },
+    { x: dayjs("2020-02-26"), y: 52 },
+    { x: dayjs("2020-03-27"), y: 50 },
+    { x: dayjs("2020-04-28"), y: 55 },
+    { x: dayjs("2020-05-29"), y: 55 },
+    { x: dayjs("2020-06-30"), y: 60 },
+    { x: dayjs("2020-07-31"), y: 60 },
+    { x: dayjs("2020-09-01"), y: 55 },
+    { x: dayjs("2020-10-02"), y: 65 },
+    { x: dayjs("2020-11-03"), y: 70 },
+    { x: dayjs("2020-12-04"), y: 70 },
+    { x: dayjs("2020-12-30"), y: 75 },
+  ]
+
+  const tickValues = [data[0].x, data[data.length - 1].x]
+  const tickFormat = (aDate: dayjs.Dayjs) => aDate.format("YYYY/MM/DD")
+
+  return (
+    <VictoryChart>
+      <VictoryAxis tickValues={tickValues} tickFormat={tickFormat} />
+      <VictoryAxis dependentAxis domain={[40, 90]} />
       <VictoryLine data={data} x="x" y="y" />
     </VictoryChart>
   )
@@ -70,6 +109,7 @@ export const GraphScreen: React.FC = () => {
       <MarginDiveder />
       <VictoryDateAxis />
       <MarginDiveder />
+      <VictorySampleData />
     </ScrollView>
   )
 }
