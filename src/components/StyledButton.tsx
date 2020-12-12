81977242f0915f5b362cd820ac0c67dd25a3c045
Shinichi Kawamura <sttaf34@gmail.com>
2020-12-12 18:41:33 +0900

    ボタンのコンポーネント追加

---
+++ src/components/StyledButton.tsx
@@ -0,0 +1,47 @@
+import React from "react"
+import {
+  Text,
+  Pressable,
+  StyleSheet,
+  PressableStateCallbackType,
+} from "react-native"
+
+const styles = StyleSheet.create({
+  button: {
+    margin: 12,
+    borderWidth: 0.5,
+    borderRadius: 4,
+  },
+  buttonPressed: {
+    margin: 12,
+    borderWidth: 0.5,
+    borderRadius: 4,
+    backgroundColor: "#6bb6ff",
+  },
+  text: {
+    padding: 12,
+  },
+})
+
+const style = (state: PressableStateCallbackType) => {
+  return state.pressed ? styles.buttonPressed : styles.button
+}
+
+type Props = {
+  title: string
+  onPress: () => void
+}
+
+export const StyledButton: React.FC<Props> = (props: Props) => {
+  const { title, onPress } = props
+  const onPressAnimation = () => {
+    requestAnimationFrame(() => {
+      onPress()
+    })
+  }
+  return (
+    <Pressable style={style} onPress={onPressAnimation}>
+      <Text style={styles.text}>{title}</Text>
+    </Pressable>
+  )
+}
