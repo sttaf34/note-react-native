6f5e8cd42ebddbf0ae4f0d318c5a092eae91c298
Shinichi Kawamura <sttaf34@gmail.com>
2021-03-23 19:20:03 +0900

    ダウンロードした画像の共有のサンプルを追加

--- src/screens/other/ShareFileScreen.tsx
+++ src/screens/other/ShareFileScreen.tsx
@@ -5,15 +5,6 @@ import * as FileSystem from "expo-file-system"
 import { StyledButton } from "src/components/StyledButton"
 
 export const ShareFileScreen: React.FC = () => {
-  const writeFile = () => {
-    const { documentDirectory } = FileSystem
-    if (documentDirectory) {
-      const fileUri = `${FileSystem.documentDirectory}sample.txt`
-      console.log(fileUri)
-      FileSystem.writeAsStringAsync(fileUri, "ABCDEFG")
-    }
-  }
-
   const readDirectory = async () => {
     const { documentDirectory } = FileSystem
     if (documentDirectory) {
@@ -22,16 +13,40 @@ export const ShareFileScreen: React.FC = () => {
     }
   }
 
-  const share = () => {
+  const writeFile = () => {
     const fileUri = `${FileSystem.documentDirectory}sample.txt`
+    console.log(fileUri)
+    FileSystem.writeAsStringAsync(fileUri, "ABCDEFG")
+  }
+
+  const shareTextFile = () => {
+    const fileUri = `${FileSystem.documentDirectory}sample.txt`
+    Sharing.shareAsync(fileUri)
+  }
+
+  const downloadFile = () => {
+    const url = "https://sttaf34.net/images/0/2.png"
+    const fileUri = `${FileSystem.documentDirectory}download.png`
+    const option: FileSystem.DownloadOptions = {
+      sessionType: FileSystem.FileSystemSessionType.FOREGROUND,
+    }
+    FileSystem.downloadAsync(url, fileUri, option)
+  }
+
+  const shareImageFile = () => {
+    const fileUri = `${FileSystem.documentDirectory}download.png`
     Sharing.shareAsync(fileUri)
   }
 
   return (
     <>
-      <StyledButton title="WRITE" onPress={writeFile} />
       <StyledButton title="READ" onPress={readDirectory} />
-      <StyledButton title="INTENT" onPress={share} />
+
+      <StyledButton title="WRITE" onPress={writeFile} />
+      <StyledButton title="INTENT" onPress={shareTextFile} />
+
+      <StyledButton title="DOWNLOAD" onPress={downloadFile} />
+      <StyledButton title="INTENT" onPress={shareImageFile} />
     </>
   )
 }
